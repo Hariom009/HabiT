581d4e24f284a4ba45d2bd58ded1432ac79361fb
Hariom009 <hariomconnects03@gmail.com>
2025-03-19 19:24:40 +0530

    Initial Commit

---
+++ HabiT/HabiTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTApp.swift
+//  HabiT
+//
+//  Created by Hari's Mac on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabiTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabiTTests/HabiTTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTTests.swift
+//  HabiTTests
+//
+//  Created by Hari's Mac on 19.03.2025.
+//
+
+import Testing
+@testable import HabiT
+
+struct HabiTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
